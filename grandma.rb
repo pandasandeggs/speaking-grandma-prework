ceb030b5df1445a41dbbcb1884faa82c23031e4e
Chantal Wallace <theteatreehouse@gmail.com>
2018-06-07 01:00:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
     
   elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!
+    return "I LOVE YOU TOO PUMPKIN!"
   end 
     
 end
