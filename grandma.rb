65823ac59ded6b06daba2a2e6d0efd5eee336081
Chantal Wallace <theteatreehouse@gmail.com>
2018-06-07 00:23:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
-def speak_to_grandma(string)
-  if string == string.downcase 
+def speak_to_grandma(greeting)
+  if greeting == greeting.downcase 
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif string == string.upcase
+  elsif greeting == greeting.upcase
     puts "NO, NOT SINCE 1983!"
-  else string = "I LOVE YOU GRANDMA!"
+  else greeting = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
 end
